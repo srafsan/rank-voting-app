1026de0fef578f016fc828c88d7682968964cc51
srafsan <srafsan98@tutanota.com>
2024-11-05 08:07:50 +0600

    creating controllers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PollsModule } from './polls/polls.module';
+import { PollsController } from './polls/polls.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [],
+  imports: [ConfigModule.forRoot(), PollsModule],
+  controllers: [PollsController],
   providers: [],
 })
 export class AppModule {}
